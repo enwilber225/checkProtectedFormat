8da09a88b44296a8705efc2f258912687f66eea3
enwilber225 <159064049+enwilber225@users.noreply.github.com>
2024-12-09 20:39:30 -0700

    Add files via upload
    It's not much, but it's honest work.

---
+++ checkProtectedFormat.py
@@ -0,0 +1,12 @@
+# Elias Wilber 
+# CIS 129
+# 12/09/2024
+# 12937
+
+
+print()
+amount = float(input("Enter a dollar amount, as a float.\n\nPlease enter here: "))
+print()
+
+print(f"{amount:*>10,.2f}")
+print()
